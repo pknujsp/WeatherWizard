6c1433aa0874ac77102ecaca3e94a8ed50dee05d
JSPark <48265129+pknujsp@users.noreply.github.com>
2023-10-23 20:28:55 +0900

    앱 위젯 모듈 생성, 종합 위젯 정보 추가

---
+++ feature/widget/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    id("plugin.android.feature")
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "io.github.pknujsp.weatherwizard.feature.widget"
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+    implementation(project(":core:domain"))
+    implementation(project(":core:data"))
+    implementation(project(":feature:favorite"))
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.bundles.workmanager)
+
+    ksp(libs.ksealedbinding.compiler)
+    implementation(libs.ksealedbinding.annotation)
+}
