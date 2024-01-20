4c0c50a2e13e45bae287eb824b4c59fbdd8a58df
JSPark <48265129+pknujsp@users.noreply.github.com>
2024-01-20 21:40:36 +0900

    지도 레이더 타일 캐싱 기능 로직 개선 : 기존에는 타일 오버레이를 만들 때마다 캐시를 지웠음 -> 개선 : savedStateHandle에 임시 저장해서 효율 개선함

--- core/model/src/main/java/io/github/pknujsp/weatherwizard/core/model/rainviewer/RadarTiles.kt
+++ core/model/src/main/java/io/github/pknujsp/weatherwizard/core/model/rainviewer/RadarTiles.kt
@@ -1,19 +1,17 @@
 package io.github.pknujsp.weatherwizard.core.model.rainviewer
 
-import io.github.pknujsp.weatherwizard.core.model.UiModel
-import kotlinx.serialization.SerialName
-import kotlinx.serialization.Serializable
+import io.github.pknujsp.weatherwizard.core.model.EntityModel
 import java.time.ZonedDateTime
 
 
 data class RadarTiles(
     val generated: Int,
     val host: String,
-    val currentIndex:Int,
+    val currentIndex: Int,
     val radar: List<Data>,
     val version: String,
     val requestTime: ZonedDateTime = ZonedDateTime.now()
-) : UiModel {
+) : EntityModel {
 
     data class Data(
         val path: String, val time: Int
