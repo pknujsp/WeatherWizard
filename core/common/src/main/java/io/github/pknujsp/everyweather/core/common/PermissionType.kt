f27848aa336c403798c44718d2f5a3a60c096045
JSPark <48265129+pknujsp@users.noreply.github.com>
2024-02-03 22:46:09 +0900

    포그라운드 서비스 위치 권한 추가

--- core/common/src/main/java/io/github/pknujsp/everyweather/core/common/PermissionType.kt
+++ core/common/src/main/java/io/github/pknujsp/everyweather/core/common/PermissionType.kt
@@ -9,8 +9,10 @@ import androidx.annotation.RequiresApi
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 
-enum class PermissionType(val permissions: Array<String>, val isUnrelatedSdkDevice: Boolean) : StatefulFeature {
-    LOCATION(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION), isUnrelatedSdkDevice = false) {
+enum class PermissionType(val permissions: Array<String>, val isUnrelatedSdkDevice: Boolean, val reason: Int) : StatefulFeature {
+    LOCATION(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION),
+        false,
+        io.github.pknujsp.everyweather.core.resource.R.string.location_permission_description) {
         override val title: Int = io.github.pknujsp.everyweather.core.resource.R.string.location_permission
         override val message: Int = io.github.pknujsp.everyweather.core.resource.R.string.location_permission_denied
         override val action: Int = io.github.pknujsp.everyweather.core.resource.R.string.grant_permissions
@@ -19,7 +21,10 @@ enum class PermissionType(val permissions: Array<String>, val isUnrelatedSdkDevi
     },
 
     @RequiresApi(Build.VERSION_CODES.UPSIDE_DOWN_CAKE)
-    FOREGROUND_SERVICE_LOCATION(arrayOf(Manifest.permission.FOREGROUND_SERVICE_LOCATION), isUnrelatedSdkDevice = false) {
+    FOREGROUND_SERVICE_LOCATION(
+        arrayOf(Manifest.permission.FOREGROUND_SERVICE_LOCATION), Build.VERSION.SDK_INT < Build.VERSION_CODES.UPSIDE_DOWN_CAKE,
+        io.github.pknujsp.everyweather.core.resource.R.string.foreground_service_location_permission_description,
+    ) {
         override val title: Int = io.github.pknujsp.everyweather.core.resource.R.string.foreground_service_location_permission
         override val message: Int = io.github.pknujsp.everyweather.core.resource.R.string.foreground_service_location_permission_denied
         override val action: Int = io.github.pknujsp.everyweather.core.resource.R.string.grant_permissions
@@ -29,7 +34,8 @@ enum class PermissionType(val permissions: Array<String>, val isUnrelatedSdkDevi
 
     @RequiresApi(Build.VERSION_CODES.Q)
     BACKGROUND_LOCATION(arrayOf(Manifest.permission.ACCESS_BACKGROUND_LOCATION),
-        isUnrelatedSdkDevice = Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+        Build.VERSION.SDK_INT < Build.VERSION_CODES.Q,
+        io.github.pknujsp.everyweather.core.resource.R.string.background_location_permission_description) {
         override val title: Int = io.github.pknujsp.everyweather.core.resource.R.string.background_location_permission
         override val message: Int = io.github.pknujsp.everyweather.core.resource.R.string.background_location_permission_denied
         override val action: Int = io.github.pknujsp.everyweather.core.resource.R.string.grant_permissions
@@ -39,7 +45,8 @@ enum class PermissionType(val permissions: Array<String>, val isUnrelatedSdkDevi
 
     @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     POST_NOTIFICATIONS(arrayOf(Manifest.permission.POST_NOTIFICATIONS),
-        isUnrelatedSdkDevice = Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU) {
+        Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU,
+        io.github.pknujsp.everyweather.core.resource.R.string.post_notification_permission_description) {
         override val title: Int = io.github.pknujsp.everyweather.core.resource.R.string.post_notification_permission
         override val message: Int = io.github.pknujsp.everyweather.core.resource.R.string.post_notification_permission_denied
         override val action: Int = io.github.pknujsp.everyweather.core.resource.R.string.grant_permissions
@@ -48,9 +55,9 @@ enum class PermissionType(val permissions: Array<String>, val isUnrelatedSdkDevi
     },
 
     @RequiresApi(Build.VERSION_CODES.S)
-    SCHEDULE_EXACT_ALARM_ABOVE_EQUALS_ON_SDK_31(arrayOf(
-        Manifest.permission.SCHEDULE_EXACT_ALARM,
-    ), isUnrelatedSdkDevice = Build.VERSION.SDK_INT < Build.VERSION_CODES.S) {
+    SCHEDULE_EXACT_ALARM_ABOVE_EQUALS_ON_SDK_31(arrayOf(Manifest.permission.SCHEDULE_EXACT_ALARM),
+        Build.VERSION.SDK_INT < Build.VERSION_CODES.S,
+        io.github.pknujsp.everyweather.core.resource.R.string.exact_alarm_permission_description) {
         override val title: Int = io.github.pknujsp.everyweather.core.resource.R.string.exact_alarm_permission
         override val message: Int = io.github.pknujsp.everyweather.core.resource.R.string.exact_alarm_permission_denied
         override val action: Int = io.github.pknujsp.everyweather.core.resource.R.string.grant_permissions
@@ -71,6 +78,6 @@ fun Activity.shouldShowRequestPermissionRationale(permissionType: PermissionType
  * 해당 권한을 고려할 필요가 없는 SDK 버전이라면 즉시 true를 반환
  */
 fun Context.checkSelfPermission(permissionType: PermissionType): Boolean =
-    permissionType.isUnrelatedSdkDevice or permissionType.permissions.all {
+    permissionType.isUnrelatedSdkDevice || permissionType.permissions.all {
         ContextCompat.checkSelfPermission(this, it) == PackageManager.PERMISSION_GRANTED
     }
