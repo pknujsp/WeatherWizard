d6d81258864986794654f7a1200a43932c837f88
JSPark <48265129+pknujsp@users.noreply.github.com>
2024-03-21 01:52:25 +0900

    앱 버전 5 -> 6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "io.github.pknujsp.everyweather"
-        versionCode = 5
+        versionCode = 6
         versionName = "1.0"
 
         vectorDrawables {
