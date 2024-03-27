3c6566a370d641781deae41cdb59162775464b61
JSPark <48265129+pknujsp@users.noreply.github.com>
2024-03-27 16:23:00 +0900

    앱 버전 코드 9 -> 10, AI요약 결과 출력 시 16MS 차이로 한 문자씩 나타내도록 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "io.github.pknujsp.everyweather"
-        versionCode = 9
+        versionCode = 10
         versionName = "1.0"
 
         vectorDrawables {
--- core/ui/src/main/java/io/github/pknujsp/everyweather/core/ui/PlaceHolder.kt
+++ core/ui/src/main/java/io/github/pknujsp/everyweather/core/ui/PlaceHolder.kt
@@ -13,48 +13,42 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 
-private const val SHADOW_BRUSH_WIDTH = 550
-private const val DURATION = 800
+private const val SHADOW_BRUSH_WIDTH = 500
+private const val DURATION = 900
 private const val ANGLE_OF_AXIS_Y = 265f
-
 private const val LABEL = "Shimmer animation"
 
-private val shimmerColors =
-    listOf(
-        Color.White.copy(alpha = 0.3f),
-        Color.White.copy(alpha = 0.5f),
-        Color.White.copy(alpha = 1.0f),
-        Color.White.copy(alpha = 0.5f),
-        Color.White.copy(alpha = 0.3f),
+private val shimmerColors = listOf(
+    Color.White.copy(alpha = 0.3f),
+    Color.White.copy(alpha = 0.5f),
+    Color.White.copy(alpha = 0.75f),
+    Color.White.copy(alpha = 1.0f),
+    Color.White.copy(alpha = 0.75f),
+    Color.White.copy(alpha = 0.5f),
+    Color.White.copy(alpha = 0.3f),
+)
+
+
+fun Modifier.shimmer(): Modifier = composed {
+    val transition = rememberInfiniteTransition(label = "")
+    val translateAnimation = transition.animateFloat(
+        initialValue = 0f,
+        targetValue = (DURATION + SHADOW_BRUSH_WIDTH).toFloat(),
+        animationSpec = infiniteRepeatable(
+            animation = tween(
+                durationMillis = DURATION,
+                easing = LinearEasing,
+            ),
+            repeatMode = RepeatMode.Restart,
+        ),
+        label = LABEL,
     )
 
-
-
-fun Modifier.shimmer(): Modifier =
-    composed {
-        val transition = rememberInfiniteTransition(label = "")
-        val translateAnimation =
-            transition.animateFloat(
-                initialValue = 0f,
-                targetValue = (DURATION + SHADOW_BRUSH_WIDTH).toFloat(),
-                animationSpec =
-                    infiniteRepeatable(
-                        animation =
-                            tween(
-                                durationMillis = DURATION,
-                                easing = LinearEasing,
-                            ),
-                        repeatMode = RepeatMode.Reverse,
-                    ),
-                label = LABEL,
-            )
-
-        this.background(
-            brush =
-                Brush.linearGradient(
-                    colors = shimmerColors,
-                    start = Offset(x = translateAnimation.value - SHADOW_BRUSH_WIDTH, y = 0.0f),
-                    end = Offset(x = translateAnimation.value, y = ANGLE_OF_AXIS_Y),
-                ),
-        )
-    }
+    this.background(
+        brush = Brush.linearGradient(
+            colors = shimmerColors,
+            start = Offset(x = translateAnimation.value - SHADOW_BRUSH_WIDTH, y = 0.0f),
+            end = Offset(x = translateAnimation.value, y = ANGLE_OF_AXIS_Y),
+        ),
+    )
+}
